79a3f0304af06fea83f2e31bb99f58966d68a7fa
DavidPayan8 <davidpayanalvarado@gmail.com>
2024-10-14 09:56:34 +0200

    Update Api Methods for get Data

--- app/Http/Controllers/ArticuloController.php
+++ app/Http/Controllers/ArticuloController.php
@@ -5,57 +5,88 @@ namespace App\Http\Controllers;
 use App\Models\Articulo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ArticuloController extends Controller
 {
-
-    public function getProductsWithCustomizations()
+    public function getProductsWithCustomizations($tipo_tarifa_venta_Param)
     {
-        // Obtener productos
-        $products = DB::table('Articulos')->get();
+        $tipo_tarifa_venta = $tipo_tarifa_venta_Param ?? 2; // Aqui asignamos el tipo de tarifa que queramos. 
+        $products = DB::table('Articulos')
+                        ->join('Tarifa_Venta', 'Articulos.id', '=', 'Tarifa_Venta.articulo_id')
+                        ->select('Articulos.*', 'Tarifa_Venta.precio_venta as precio')
+                        ->where('Tarifa_Venta.tipo_tarifa_id', '=', $tipo_tarifa_venta)
+                        ->get();
+                        Log::info("Articulos: ", $products->toArray());
 
-        // Obtener preguntas de personalización
         $questions = DB::table('Preguntas_Articulo')->get();
+        // Log::info("Preguntas articulos: ", $questions->toArray());
 
-        // Obtener opciones de personalización
         $options = DB::table('Opciones_Preguntas_Articulo')->get();
-
-        // Mapear los datos
-        $customizationQuestions = $questions->map(function($question) use ($options) {
-            return [
-                'id' => $question->id,
-                'name' => $question->texto,
-                'questionType' => $question->tipo_pregunta,
-                'maxChoices' => $question->unidades_maximas,
-                'options' => $options->filter(function($option) use ($question) {
-                    return $option->pregunta_articulo_id == $question->id;
-                })->map(function($option) {
-                    return [
-                        'id' => $option->id,
-                        'value' => $option->suplemento,
-                        'img' => null, // Si tienes imágenes, debes añadir el campo de imagen
-                        'price' => null // Si tienes precios, debes añadir el campo de precio
-                    ];
-                })->toArray()
-            ];
-        });
-
-        $productsWithCustomizations = $products->map(function($product) use ($customizationQuestions) {
+        
+        $productsWithCustomizations = $products->map(function($product) use ($questions, $options) {
+            // Filtrar las preguntas que corresponden a este artículo
+            $filteredQuestions = $questions->filter(function($question) use ($product) {
+                return $question->articulo_id == $product->id;
+            });
+            // Preguntas y opciones correspondientes al artículo
+            $customizationQuestions = $filteredQuestions->map(function($question) use ($options) {
+                return [
+                    'id' => $question->id,
+                    'name' => $question->texto,
+                    'questionType' => $question->tipo_pregunta ?? "single",
+                    'maxChoices' => $question->unidades_maximas,
+                    'minChoices' => $question->unidades_minimas,
+                    'options' => $options->filter(function($option) use ($question) {
+                        // Filtrar las opciones que pertenecen a la pregunta
+                        return $option->pregunta_articulo_id == $question->id;
+                    })->map(function($option) {
+                        // Busca el producto asociado
+                        return [
+                            'id' => $option->id,
+                            'value' => $option->suplemento ?? 'well done',
+                            'img' => $product->imagen ?? null, // Si tiene una imagen, se asigna
+                            'price' => $product->precio ?? 0 // Si tiene un precio, se asigna
+                        ];
+                    })->toArray()
+                ];
+            });
+            // Retornar el producto
             return [
                 'id' => $product->id,
                 'name' => $product->articulo,
-                'price' => $product->precio, // Asegúrate de que el nombre del campo coincida
-                'taxes' => $product->tipo_iva_id, // Puedes mapear esto según tus necesidades
-                'img' => $product->imagen,
+                'price' => $product->precio,
+                'taxes' => $product->impuestos ?? 0,
+                'img' => $product->imagen ?? null,
                 'familyId' => $product->familia_id,
-                'description' => $product->descripcion, // Asegúrate de que el nombre del campo coincida
-                'customizations' => [], // Añade personalizaciones si es necesario
-                'customizationQuestions' => $customizationQuestions
+                'description' => $product->descripcion,
+                'customizationQuestions' => $customizationQuestions->toArray()
             ];
         });
 
-        return response()->json($productsWithCustomizations);
+        Log::info("PRODUCTS AUX: ", $productsWithCustomizations->toArray());
+
+
+        /* $productsWithCustomizations = $productsAux->map(function($product) use ($customizationQuestions) {
+            return [
+                'id' => $product->id,
+                'name' => $product->nombre,
+                'price' => $product->precio,
+                'taxes' => $product->tipo_iva_id,
+                'img' => $product->imagen,
+                'familyId' => $product->familia_id,
+                'description' => $product->descripcion ?? 'Comida de calidad',
+                'customizations' => [], // Añade personalizaciones si es necesario
+                'customizationQuestions' => $customizationQuestions
+            ];
+        }); */
+
+        Log::info("Productos con preguntas: ", $productsWithCustomizations->toArray());
+
+
+        return response()->json($productsWithCustomizations->toArray());
     }
+
     public function index()
     {
         $articulos = Articulo::all();
