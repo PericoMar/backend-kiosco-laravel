d2387f7694f08a758c15839878dc641ad564d143
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-11-18 18:18:24 +0100

    Update filesystem configuration; change local disk to public with updated root, URL, and visibility settings

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,9 +30,11 @@ return [
 
     'disks' => [
 
-        'local' => [
+        'public' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
             'throw' => false,
         ],
 
